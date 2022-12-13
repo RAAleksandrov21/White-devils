14792f41037b83c5346dc7118d72d860b243c1a6
EEMustafa21 <EEMustafa21@codingburgas.bg>
2022-12-14 01:44:49 +0200

    Define FindAnswer function

--- hangman/hangman/functions.cpp
+++ hangman/hangman/functions.cpp
@@ -19,3 +19,56 @@ void welcome()
     cout << "                               \\______/                                   " << endl;
     cout << "How to play: Save the man from being hanged by guessing the letters in the word.\n";
 }
+
+
+string FindAnswer(int len)
+{
+    switch (len)
+    {
+    case 1:
+        return "_";
+        break;
+    case 2:
+        return "__";
+        break;
+    case 3:
+        return "___";
+        break;
+    case 4:
+        return "____";
+        break;
+    case 5:
+        return "_____";
+        break;
+    case 6:
+        return "______";
+        break;
+    case 7:
+        return "_______";
+        break;
+    case 8:
+        return "________";
+        break;
+    case 9:
+        return "_________";
+        break;
+    case 10:
+        return "__________";
+        break;
+    case 11:
+        return "___________";
+        break;
+    case 12:
+        return "____________";
+        break;
+    case 13:
+        return "_____________";
+        break;
+    case 14:
+        return "______________";
+        break;
+    case 15:
+        return "_______________";
+        break;
+    }
+}
